74d64dcfde638560cd1bf2e0da1fbd5a83f0ae17
ss joy <ssjoy.u.b@gmail.com>
2024-01-21 02:34:54 +0600

    add test module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FormSubModule } from './form-sub/form-sub.module';
+import { TestingModule } from '@nestjs/testing';
 
 @Module({
-  imports: [FormSubModule],
+  imports: [FormSubModule, TestingModule],
   controllers: [AppController],
   providers: [AppService],
 })
