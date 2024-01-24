45799cd61846aa15eee311c8ce84f453e9186475
ss joy <ssjoy.u.b@gmail.com>
2024-01-24 16:00:20 +0600

    add: add custom logger

--- src/form-sub/form-sub.controller.ts
+++ src/form-sub/form-sub.controller.ts
@@ -1,15 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('form-sub')
 export class FormSubController {
   @Post()
-  receiveFormData(@Body() body) {
+  receiveFormData(@Body() body, @Req() req: Request) {
     const resp = `
     <h1>Form Submitted!!!!</h1>
     <p>Here is what I received</p>
-         ${JSON.stringify(body)}
+    <ul>
+      <li> ${req.hostname}</li>
+      <li> ${new Date().toLocaleDateString()}</li>
+      <li> ${new Date().toLocaleTimeString()}</li>
+      <li>${JSON.stringify(body)}</li>
+    </ul>
     `;
-
+    console.log('----------------------------');
+    console.log(resp);
+    console.log('-----------------------------');
     return resp;
   }
 }
